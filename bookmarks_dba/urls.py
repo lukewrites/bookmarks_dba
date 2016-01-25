a6972ef87d79b16c67b87aa85f582e1e3c371dc6
Luke Petschauer <luke.petschauer@gmail.com>
2016-01-24 20:40:34 -0800

    added view, url, template for image bookmarker.

--- bookmarks_dba/urls.py
+++ bookmarks_dba/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
+    url(r'^images/', include('images.urls', namespace='images')),
     url('social-auth',
         include('social.apps.django_app.urls',
                 namespace='social')),
