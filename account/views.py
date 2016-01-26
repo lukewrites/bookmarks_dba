4a4c9a57f83831b71336ec7fdb5d272d9ef2bfc2
Luke Petschauer <luke.petschauer@gmail.com>
2016-01-25 23:51:02 -0800

    ManyToManyField relationships

--- account/views.py
+++ account/views.py
@@ -3,8 +3,30 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                  'account/user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
 
 
 @login_required
@@ -75,4 +97,3 @@ def register(request):
                   'account/register.html',
                   {'user_form': user_form}
                   )
-
